579b2cb9bb8306e3630d62775c37a789696a43db
DomGibson <103586347+DomGibson@users.noreply.github.com>
2022-06-29 13:56:45 +0100

    Add files via upload

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,58 +5,33 @@ import '../App.css';
 function Post() {
 
     // const [user, setUser] = useState();
-    const [post, setPost] = useState([{
+    const [posts, setPosts] = useState([{
       username: '',
-      image: ''
+      image: '',
     }]);
   
     useEffect(() => {
       fetchPhotos();
     }, [])
-  
-    
-    // const axios = require('axios');
-    // async function doGetRequest() {
-    //   let res = await axios.get(env.MONGO_URI_PICS);
-    //   let data = res.data;
-    //   console.log(data);
-    // }
-    // doGetRequest();
 
-
-    // const fetchPhotos = async () => {
-    //   const res = await fetch(env.MONGO_URI_ONE);
-    //   const data = await res.json();
-    //   setPost(data);
-    //   console.log(data);
-    // };
-
-
-    axios.get('/post')
-    .then(function (response) {
-      // handle success
-      console.log(response);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-    });
-
-
+    const localhost = `http://localhost:${env.PORT}/posts`
+    const fetchPhotos = async () => {
+      const res = await fetch(localhost);
+      const data = await res.json();
+      setPosts(data.viewAll);
+      console.log(data.viewAll.reverse());
+    };
 
   return (
     <post className='post-container'>
         <div className='post-img'>
-            {post.map((item, index) => {
+            {posts.map((props, index) => {
                 return(
-                <div className='post-main'>
-                    <h2 id='heading2'>{item.author}</h2>
+                <div key={index} className='post-main'>
+                    <h2 id='heading2'>{props.username}</h2>
                     <img 
                     className='photo-container'
-                    src={item.image}
+                    src={props.image}
                     alt="random"/>
                 </div>
                 );
